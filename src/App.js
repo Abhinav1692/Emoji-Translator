61da6d7b54d38c83492385b7b00216d2eb0b4bf6
Abhinav1692 <89593847+Abhinav1692@users.noreply.github.com>
2021-09-11 15:56:15 +0530

    input field and emoji list created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 
-var shoppingList = ["chicken", "mutton", "eggs", "bread"];
+const emojiDictionary = {
+  // "🧀󠁧󠁢󠁥󠁮󠁧󠁢󠁥󠁮": "Chesse Wedge",
+  // "🍖󠁧󠁢󠁥󠁮󠁧󠁧󠁢󠁥󠁮󠁧": "Meat on bone",
+  "🍿": "Popcorn",
+  "🍨": "Icecream",
+  "🍣": "Sushi",
+  "🎂":
+    "Birthday cake" /** add some more to show how the app now expands when there's new data */,
+  "🥓": "Bacon",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": "Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich"
+};
+
+const emojis = Object.keys(emojiDictionary);
+//var shoppingList = ["chicken", "mutton", "eggs", "bread"];
 
 export default function App() {
+  var [emojiMeaning, setEmojiMeaning] = useState("Translation will come here");
+  var [emoji, setEmoji] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    setEmoji(userInput);
+    // if userInput in emojiDictionar
+    // var meaning;
+    if (userInput.length === 0) {
+      setEmojiMeaning("Input field is empty");
+    } else if (userInput in emojiDictionary) {
+      setEmojiMeaning(emojiDictionary[userInput]);
+    } else {
+      setEmojiMeaning("This emoji is not present in our database");
+    }
+  }
+
+  function emojiClickHandler(userInput) {
+    setEmojiMeaning(emojiDictionary[userInput]);
+  }
+
   return (
     <div className="App">
-      <h1>my shopping List</h1>
-      <ol>
-        {shoppingList.map((item, index) => {
-          if (index % 2 != 0) return <li> {item} </li>;
-          else return <li style={{ color: "gray" }}> {item} </li>;
-        })}
-      </ol>
+      <h1>Emoji Translator</h1>
+      <input
+        onChange={emojiInputHandler}
+        placeholder="search for emoji here"
+        value={emoji}
+      ></input>
+      <h3>{emoji}</h3>
+      <h3>{emojiMeaning}</h3>
+      <h3> Emojies we know</h3>
+      {
+        //mapping a listClickHandler on the list item
+        emojis.map((emoji) => (
+          <span
+            onClick={() => emojiClickHandler(emoji)} //():asynchronous func.
+            style={{ padding: "1rem", fontSize: "2rem", cursor: "pointer" }}
+          >
+            {emoji}{" "}
+          </span>
+        ))
+      }
     </div>
   );
 }
